a6753030df936123bcfca3d0527433510082b1f5
Phock <114044604+jakelequire@users.noreply.github.com>
2024-07-10 21:10:15 -0700

    ###
    before migration

---
+++ src/include/compiler/new_lexer.h
@@ -0,0 +1,125 @@
+/********************************************************************************
+ *  Copyright 2024 Jacob LeQuire                                                *
+ *  SPDX-License-Identifier: Apache-2.0                                         *  
+ *    Licensed under the Apache License, Version 2.0 (the "License");           *
+ *    you may not use this file except in compliance with the License.          * 
+ *    You may obtain a copy of the License at                                   *
+ *                                                                              *
+ *    http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                              *
+ *    Unless required by applicable law or agreed to in writing, software       *
+ *    distributed under the License is distributed on an "AS IS" BASIS,         *
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *    See the License for the specific language governing permissions and       *
+ *    limitations under the License.                                            *
+ *                                                                              *
+ ********************************************************************************/
+#ifndef LEXER_H
+#define LEXER_H
+/*------ <includes> ------*/
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <errno.h>
+#include <ctype.h>
+/*---<custom_includes>---*/
+#include "token.h"
+#include "ast.h"
+#include "logger.h"
+#include "utils/fs.h"
+/*---------<end>---------*/
+
+typedef struct {
+    char* keyword;
+    CryoTokenType type;
+} KeywordToken;
+
+typedef struct {
+    char* baseType;
+    CryoTokenType dataType;
+} DataTypeToken;
+
+typedef union {
+    int intValue;
+    float floatValue;
+    char* stringValue;
+} TokenValue;
+
+typedef struct Token {
+    CryoTokenType type;         // Type of the token
+    char* start;          // Pointer to the beginning of the token in the source code
+    int length;                 // Length of the token
+    int line;                   // Line number where the token is found
+    int column;                 // Column number where the token is found
+    const char* style;          // Style of the token
+    union {                     // Union to handle different types of token values
+        int intValue;           // Use if the token is an integer
+        float floatValue;       // Use if the token is a float
+        char* stringValue;      // Use if the token is a string
+    } value;
+} Token;
+
+typedef struct Lexer {
+    const char* source;
+    const char *start;
+    const char* end;
+    const char *current;
+    int line;
+    int column;
+    Token currentToken;
+    Token lookahead;
+    bool hasPeeked;
+} Lexer;
+
+
+/* @Util_Functions */
+char* my_strndup                            (const char* src, size_t len);
+
+
+/* @Lexer */
+void initLexer                              (Lexer* lexer, const char* source);
+void freeLexer                              (Lexer* lexer);
+
+
+/* @Lexer_Utils */
+char* advance                               (Lexer* lexer);
+bool isAtEnd                                (Lexer* lexer);
+char* peek                                  (Lexer* lexer);
+char* peekNext                              (Lexer* lexer);
+bool* matchToken                            (Lexer* lexer, CryoTokenType type);
+void skipWhitespace                         (Lexer* lexer);
+
+
+/* @Lexer_Functions */
+Token nextToken                             (Lexer* lexer, Token* token);
+Token get_next_token                        (Lexer* lexer);
+Token getToken                              (Lexer* lexer);
+Token peekToken                             (Lexer* lexer);
+Token peekNextToken                         (Lexer* lexer);
+
+
+/* @Token_Creation */
+Token makeToken                             (Lexer* lexer, CryoTokenType type);
+Token errorToken                            (Lexer* lexer, char* message);
+Token number                                (Lexer* lexer);
+Token string                                (Lexer* lexer);
+Token boolean                               (Lexer* lexer);
+Token symbolChar                            (Lexer* lexer);
+Token identifier                            (Lexer* lexer);
+
+/* @Data_Types */
+CryoTokenType checkKeyword                  (Lexer* lexer, char* keyword, CryoTokenType type);
+CryoTokenType checkDataType                 (Lexer* lexer, char* dataType, CryoTokenType type);
+
+/* @DataType_Evaluation */
+bool isAlpha                                (char* c);
+bool isDigit                                (char* c);
+bool isType                                 (char* c);
+
+/* =========================================================== */
+/* @DEBUG | Used to debug the parser in a different executable */
+int lexer(int argc, char* argv[]);
+
+#endif // LEXER_H
