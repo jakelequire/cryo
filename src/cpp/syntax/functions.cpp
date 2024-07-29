9d3a53fda574321e0355ba4aaf6fc16b77a78d25
Jake L. <jacoblequire@outlook.com>
2024-07-28 22:08:21 -0700

    ###

--- src/cpp/syntax/functions.cpp
+++ src/cpp/syntax/functions.cpp
@@ -33,40 +33,136 @@ void CryoSyntax::createDefaultMainFunction() {
 
 void CryoSyntax::generateFunctionPrototype(ASTNode* node) {
     char* functionName;
+    FunctionDeclNode* functionNode = nullptr;
+
     if(node->type == NODE_EXTERN_FUNCTION) {
         functionName = node->data.externNode.decl.function->name;
+        functionNode = node->data.externNode.decl.function;
     } else if (node->type == NODE_FUNCTION_DECLARATION) {
         functionName = node->data.functionDecl.function->name;
+        functionNode = node->data.functionDecl.function;
     } else {
         std::cerr << "[CPP] Error: Invalid function node\n";
         return;
     }
-    
-    CryoDataType ASTDataType = node->data.functionDecl.function->returnType;
+
+    std::cout << "[CPP] Generating function prototype for " << functionName << std::endl;
+
+    CryoDataType ASTDataType = functionNode->returnType;
+    std::cout << "[CPP] Function return type: " << CryoDataTypeToString(ASTDataType) << std::endl;
     llvm::Type* returnType = CryoTypes->getLLVMType(ASTDataType);
+    std::cout << "[CPP] LLVM return type: " << returnType->getTypeID() << std::endl;
     std::vector<llvm::Type*> paramTypes;
 
-    // TODO
+    std::cout << "[CPP] Function has " << functionNode->paramCount << " parameters\n";
+
+    for (int i = 0; i < functionNode->paramCount; i++) {
+        CryoDataType paramType = functionNode->params[i]->data.varDecl.dataType;
+        llvm::Type* llvmType = CryoTypes->getLLVMType(paramType);
+        paramTypes.push_back(llvmType);
+    }
+
+    llvm::FunctionType* funcType = llvm::FunctionType::get(returnType, paramTypes, false);
+    llvm::Function* function = llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, functionName, module.get());
+
+    if (node->type == NODE_EXTERN_FUNCTION) {
+        namedValues[functionName] = function;
+    }
+
+    std::cout << "[CPP] Created function prototype for " << functionName << std::endl;
 }
 
 void CryoSyntax::generateFunction(ASTNode* node) {
+    char* functionName;
+    FunctionDeclNode* functionNode = nullptr;
 
+    if (node->type == NODE_EXTERN_STATEMENT) {
+        functionName = node->data.externNode.decl.function->name;
+        functionNode = node->data.externNode.decl.function;
+    } else if (node->type == NODE_FUNCTION_DECLARATION) {
+        functionName = node->data.functionDecl.function->name;
+        functionNode = node->data.functionDecl.function;
+    } else {
+        std::cerr << "[CPP] Error: Invalid function node\n";
+        return;
+    }
+
+    llvm::Function* function = module->getFunction(functionName);
+    if (!function) {
+        std::cerr << "[CPP] Error: Function not found in module\n";
+        return;
+    }
+
+    llvm::BasicBlock* entry = llvm::BasicBlock::Create(module->getContext(), "entry", function);
+    builder.SetInsertPoint(entry);
+
+    // Create a new scope for the function
+    namedValues.clear();
+
+    for (int i = 0; i < functionNode->paramCount; i++) {
+        llvm::Argument* arg = function->arg_begin() + i;
+        arg->setName(functionNode->params[i]->data.varDecl.name);
+        namedValues[functionNode->params[i]->data.varDecl.name] = arg;
+    }
+
+    generateFunctionBlock(functionNode->body);
+
+    llvm::verifyFunction(*function);
+    std::cout << "[CPP] Generated function " << functionName << std::endl;
+
+    if (node->type == NODE_EXTERN_STATEMENT) {
+        namedValues.erase(functionName);
+    }
+
+    return;
 }
 
 void CryoSyntax::generateExternalDeclaration(ASTNode* node) {
+    if (node->type == NODE_EXTERN_FUNCTION) {
+        generateFunctionPrototype(node);
+    } else {
+        std::cerr << "[CPP] Error: Invalid external declaration node\n";
+    }
 
+    return;
 }
 
 void CryoSyntax::generateReturnStatement(ASTNode* node) {
+    if (!node->data.returnStmt.expression) {
+        builder.CreateRetVoid();
+        return;
+    }
 
+    llvm::Value* returnValue = generateExpression(node->data.returnStmt.expression);
+    builder.CreateRet(returnValue);
+
+    return;
 }
 
 void CryoSyntax::generateFunctionCall(ASTNode* node) {
+    llvm::Function* function = module->getFunction(node->data.functionCall.name);
+    if (!function) {
+        std::cerr << "[CPP] Error: Function not found in module\n";
+        return;
+    }
 
+    std::vector<llvm::Value*> args;
+    for (int i = 0; i < node->data.functionCall.argCount; i++) {
+        args.push_back(generateExpression(node->data.functionCall.args[i]));
+    }
+
+    llvm::Value* call = builder.CreateCall(function, args);
+    std::cout << "[CPP] Generated function call to " << node->data.functionCall.name << std::endl;
+
+    return;
 }
 
 void CryoSyntax::generateFunctionBlock(ASTNode* node) {
+    for (int i = 0; i < node->data.functionBlock.block->data.block.stmtCount; i++) {
+        generateStatement(node->data.functionBlock.block->data.block.statements[i]);
+    }
 
+    return;
 }
 
 
--- src/cpp/syntax/variables.cpp
+++ src/cpp/syntax/variables.cpp
@@ -18,15 +18,107 @@
 
 namespace Cryo {
 
-void CryoSyntax::generateVarDeclaration(ASTNode* node) {}
-llvm::Value* CryoSyntax::lookupVariable(char* name) {}
-llvm::Value* CryoSyntax::createVariableDeclaration(ASTNode* node) {}
-llvm::Value* CryoSyntax::getVariableValue(char* name) {}
-llvm::GlobalVariable* CryoSyntax::createGlobalVariable(llvm::Type* varType, llvm::Constant* initialValue, char* varName) {}
-llvm::Value* CryoSyntax::loadGlobalVariable(llvm::GlobalVariable* globalVar, char* name) {}
+void CryoSyntax::generateVarDeclaration(ASTNode* node) {
+    char* varName = node->data.varDecl.name;
+    CryoDataType varType = node->data.varDecl.dataType;
+    llvm::Type* llvmType = CryoTypes->getLLVMType(varType);
 
-void CryoSyntax::generateArrayLiteral(ASTNode* node) {}
-std::vector<llvm::Constant*> CryoSyntax::generateArrayElements(ASTNode* arrayLiteral) {}
+    llvm::Value* initialValue = nullptr;
+    if (node->data.varDecl.initializer) {
+        initialValue = generateExpression(node->data.varDecl.initializer);
+    }
 
+    llvm::Value* var = createVariableDeclaration(node);
+    namedValues[varName] = var;
+
+    if (initialValue) {
+        builder.CreateStore(initialValue, var);
+    }
+
+    std::cout << "[CPP] Generated variable declaration for " << varName << std::endl;
 }
 
+
+llvm::Value* CryoSyntax::lookupVariable(char* name) {
+    llvm::Value* var = namedValues[name];
+    if (!var) {
+        std::cerr << "[CPP] Error: Variable " << name << " not found\n";
+    }
+    return var;
+}
+
+
+llvm::Value* CryoSyntax::createVariableDeclaration(ASTNode* node) {
+    char* varName = node->data.varDecl.name;
+    CryoDataType varType = node->data.varDecl.dataType;
+    llvm::Type* llvmType = CryoTypes->getLLVMType(varType);
+
+    llvm::Value* var = nullptr;
+    if (node->data.varDecl.isGlobal) {
+        llvm::Constant* initialValue = nullptr;
+        if (node->data.varDecl.initializer) {
+            initialValue = (llvm::Constant*)generateExpression(node->data.varDecl.initializer);
+        }
+        var = createGlobalVariable(llvmType, initialValue, varName);
+    } else {
+        var = builder.CreateAlloca(llvmType, nullptr, varName);
+    }
+
+    return var;
+}
+
+
+llvm::Value* CryoSyntax::getVariableValue(char* name) {
+    llvm::Value* var = lookupVariable(name);
+    if (!var) {
+        return nullptr;
+    }
+    llvm::Type* varType = var->getType();
+    return builder.CreateLoad(varType, var, name);
+}
+
+
+llvm::GlobalVariable* CryoSyntax::createGlobalVariable(llvm::Type* varType, llvm::Constant* initialValue, char* varName) {
+    llvm::GlobalVariable* globalVar = new llvm::GlobalVariable(*module, varType, false, llvm::GlobalValue::ExternalLinkage, initialValue, varName);
+    return globalVar;
+}
+
+
+llvm::Value* CryoSyntax::loadGlobalVariable(llvm::GlobalVariable* globalVar, char* name) {
+    llvm::Type* varType = globalVar->getType();
+    return builder.CreateLoad(varType, globalVar, name);
+}
+
+
+void CryoSyntax::generateArrayLiteral(ASTNode* node) {
+    char* varName = node->data.varDecl.name;
+    CryoDataType varType = node->data.varDecl.dataType;
+    llvm::Type* llvmType = CryoTypes->getLLVMType(varType);
+
+    llvm::Value* var = nullptr;
+    if (node->data.varDecl.isGlobal) {
+        llvm::Constant* initialValue = nullptr;
+        if (node->data.varDecl.initializer) {
+            initialValue = (llvm::Constant*)generateExpression(node->data.varDecl.initializer);
+        }
+        var = createGlobalVariable(llvmType, initialValue, varName);
+    } else {
+        var = builder.CreateAlloca(llvmType, nullptr, varName);
+    }
+
+    namedValues[varName] = var;
+}
+
+
+std::vector<llvm::Constant*> CryoSyntax::generateArrayElements(ASTNode* arrayLiteral) {
+    std::vector<llvm::Constant*> elements;
+    for (int i = 0; i < arrayLiteral->data.arrayLiteral.elementCount; i++) {
+        ASTNode* element = arrayLiteral->data.arrayLiteral.elements[i];
+        llvm::Constant* llvmElement = (llvm::Constant*)generateExpression(element);
+        elements.push_back(llvmElement);
+    }
+    return elements;
+}
+
+
+} // namespace Cryo
